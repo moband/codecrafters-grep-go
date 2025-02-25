d4f7e31c8c3f098c14c9d7822fda2327a30f0dd5
MohammadReza Alagheband <alagheband@live.com>
2025-02-25 14:10:59 +0100

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -5,11 +5,9 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"unicode"
 	"unicode/utf8"
 )
-
-// Ensures gofmt doesn't remove the "bytes" import above (feel free to remove this!)
-var _ = bytes.ContainsAny
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 func main() {
@@ -20,7 +18,7 @@ func main() {
 
 	pattern := os.Args[2]
 
-	line, err := io.ReadAll(os.Stdin) // assume we're only dealing with a single line
+	line, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
 		os.Exit(2)
@@ -36,21 +34,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	// default exit code is 0 which means success
 }
 
 func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) != 1 {
-		return false, fmt.Errorf("unsupported pattern: %q", pattern)
-	}
-
 	var ok bool
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+	if pattern == "\\d" {
+		ok = bytes.ContainsFunc(line, unicode.IsDigit)
+	} else {
 
-	// Uncomment this to pass the first stage
-	ok = bytes.ContainsAny(line, pattern)
+		if utf8.RuneCountInString(pattern) != 1 {
+			return false, fmt.Errorf("unsupported pattern: %q", pattern)
+		}
 
+		ok = bytes.ContainsAny(line, pattern)
+
+	}
 	return ok, nil
 }
